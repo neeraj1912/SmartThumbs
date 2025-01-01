a9af3f03bf289b1ecdbbf77f6bb649fb29650309
Neeraj Choudhary <nc28778@gmail.com>
2025-01-01 22:08:47 +0530

    fixed

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,15 +1,5 @@
 "use client"
 import React from 'react'
-<<<<<<< HEAD
-import Navbar from "./navbar"
-import Hero from "./heroSection"
-import AnimatedBeamMultipleOutputDemo from "./content"
-import LinkPreviewDemo from "./preview"
-import AppleCardsCarouselDemo from "./appleCarousel"
-import AnimatedTestimonialsDemo from './testimonials'
-import Footer from "./footer"  // Note the capital F
-import MacbookScrollDemo  from "./macbookScroll";
-=======
 import Navbar from "./navbar2"
 import Hero from "./heroSection2"
 import AnimatedBeamMultipleOutputDemo from "./content2"
@@ -17,17 +7,12 @@ import LinkPreviewDemo from "./preview2"
 import AppleCardsCarouselDemo from "./appleCarousel2"
 import AnimatedTestimonialsDemo from './testimonials2'
 import Footer from "./footer2"  // Note the capital F
->>>>>>> 3a3449b7442b0d31e0de948d01d1e598efb6dc56
 
 const Home = () => {
   return (
     <div className='bg-black text-white min-h-screen'>
-<<<<<<< HEAD
-      
-=======
 
       <h1>thank you mam for taking our review</h1>
->>>>>>> 3a3449b7442b0d31e0de948d01d1e598efb6dc56
       <Navbar />
       <Hero />
       <AnimatedBeamMultipleOutputDemo/>
